6cbc6916734e59626fcab9d977ce4971c1cbae27
kamil10x <shaik.kamil10@gmail.com>
2026-09-05 10:49:19 +0530

    All setup configuered

--- src/app.js
+++ src/app.js
@@ -22,5 +22,12 @@ app.use(urlencoded({
 
 app.use(cookieParser());
 
+//import routs
+import userRouter from "./routes/user.routes.js"
+
+
+//route declaration
+
+app.use("/api/v1/users", userRouter);
 
 export { app }
